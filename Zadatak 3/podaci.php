f3f37239d04f31b021ddadabc2540cbdb4dc1e51
Filip Janješić <filip.janjesic@gmail.com>
2024-03-01 23:39:11 +0100

    Create podaci.php

---
+++ Zadatak 3/podaci.php
@@ -0,0 +1,16 @@
+<?php
+$brojrezervacije = "2022-16950-63";
+$usluga = "AS-16950-a";
+$cijenaPoNocenju = 104.00;
+$kolicinaOsoba = 2;
+$brojNocenja = 7;
+$ukupnoUsluga = $cijenaPoNocenju * $kolicinaOsoba * $brojNocenja;
+
+$akontacijaNačinPlaćanja = "Kreditnom karticom (Visa. EC/MC. Maestro)";
+$akontacijaVrijemePlaćanja = "Najkasnije 08.08.2022 do 11:00";
+$akontacijaIznos = 364.00;
+
+$ostatakNačinPlaćanja = "Kreditnom karticom (Visa. EC/MC. Maestro)";
+$ostatakVrijemePlaćanja = "Najkasnije 08.08.2022 do 15:00";
+$ostatakIznos = 364.00;
+?>
